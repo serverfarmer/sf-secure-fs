79d27f170233c4402a5cba0b583be8494e9c9190
Tomasz Klim <github@tomaszklim.pl>
2015-12-24 13:56:03 +0100

    run main script once right after setup

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ if ! grep -q /opt/sf-secure-fs/cron/check.sh /etc/crontab; then
 	sed -i -e "/scripts\/check\/security.sh/d" /etc/crontab
 	echo "22 1 * * * root /opt/sf-secure-fs/cron/check.sh" >>/etc/crontab
 fi
+
+/opt/sf-secure-fs/cron/check.sh
