a825848a4bd0c3932d172006191a02270a51a10a
Tomasz Klim <github@tomaszklim.pl>
2016-10-19 00:41:20 +0200

    disable extension functionality on netbsd

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+. /opt/farm/scripts/init
 
-if ! grep -q /opt/farm/ext/secure-fs/cron/check.sh /etc/crontab; then
+if ! grep -q /opt/farm/ext/secure-fs/cron/check.sh /etc/crontab && [ "$OSTYPE" != "netbsd" ]; then
 	sed -i -e "/scripts\/check\/security.sh/d" /etc/crontab
 	echo "22 1 * * * root /opt/farm/ext/secure-fs/cron/check.sh" >>/etc/crontab
 fi
