7b52157604ab4a5095062a2f5a60e62a75eeaec7
Tomasz Klim <github@tomaszklim.pl>
2018-03-19 01:13:42 +0100

    automated detection of /srv/mounts subdirectories

--- cron/check.sh
+++ cron/check.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # This script periodically checks and (if needed) restores secure permissions
 # on directories, whose contents shouldn't be visible to ordinary users.
-# Tomasz Klim, Aug 2014, Jan 2015, Jan 2018
+# Tomasz Klim, Aug 2014, Jan 2015, Jan 2018, Mar 2018
 
 
-chmod a-rwx /usr/lib/gcc/* 2>/dev/null
+chmod o-rwx /usr/lib/gcc/* 2>/dev/null
 chmod 0700 /srv/rsync/* 2>/dev/null
 chmod 0711 /srv/sites/* 2>/dev/null
+chmod 0711 /srv/mounts/* 2>/dev/null
 chmod 0711 /srv/isync/* 2>/dev/null
 chmod 0711 /srv/imap/* 2>/dev/null
 chmod 0711 /home/* 2>/dev/null
@@ -48,31 +49,15 @@ GENERIC=(
 	/srv/cifs
 	/srv/imap
 	/srv/isync
+	/srv/mounts
+	/srv/nfs
 	/srv/rsync
 	/srv/sites
-	/srv/mounts
-	/srv/mounts/backup
-	/srv/mounts/internal1
-	/srv/mounts/internal2
-	/srv/mounts/internal3
-	/srv/mounts/internal4
-	/srv/mounts/shadow1
-	/srv/mounts/shadow2
-	/srv/mounts/shadow3
-	/srv/mounts/shadow4
 	/var/backups
 	/var/cache
 	/var/lib
 	/var/spool
 	/var/www
-)
-
-SAMBA=(
-	/srv/mounts/internal1
-	/srv/mounts/internal2
-	/srv/mounts/internal3
-	/srv/mounts/internal4
-	/srv/mounts/moosefs
 )
 
 
@@ -92,7 +77,7 @@ for D in ${GENERIC[@]}; do
 	fi
 done
 
-for D in ${SAMBA[@]}; do
+for D in `ls -d /srv/mounts/* 2>/dev/null |grep -v external`; do
 	for P in `ls -d $D/samba* 2>/dev/null`; do
 		perm=`stat -c %a:%U:%G $P`
 		if [ "$perm" != "750:root:sambashare" ]; then
